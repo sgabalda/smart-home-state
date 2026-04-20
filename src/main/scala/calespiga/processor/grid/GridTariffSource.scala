96a8fdbc713e5997bb370b41c2b768b9947d92f7
Sergi Gabaldà <sergigabol@gmail.com>
2026-04-20 21:59:37 +0200

    [CHORE] Logs on tariff change (#125)
    * Logs on tariff change to identify problem
    
    * fmt
    
    ---------
    
    Co-authored-by: s.gabalda <s.gabalda@stuart.com>

--- src/main/scala/calespiga/processor/grid/GridTariffSource.scala
+++ src/main/scala/calespiga/processor/grid/GridTariffSource.scala
@@ -5,12 +5,16 @@ import cats.effect.IO
 import fs2.Stream
 import java.time.ZoneId
 import scala.concurrent.duration.*
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 trait GridTariffSource {
   def events: Stream[IO, Event.Grid.GridTariffChanged]
 }
 
 object GridTariffSource {
+
+  private given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
   private final case class Impl(zone: ZoneId) extends GridTariffSource {
 
@@ -25,9 +29,16 @@ object GridTariffSource {
             now <- IO.realTimeInstant
             next = GridTariff.nextChangeInstant(now, zone)
             delay = math.max(0L, java.time.Duration.between(now, next).toMillis)
+            _ <- logger.info(
+              s"At ${now.atZone(zone)}, next grid tariff change at ${next.atZone(zone)} (in ${delay.millis})"
+            )
             _ <- IO.sleep(delay.millis)
             t <- IO.realTimeInstant
-          } yield Event.Grid.GridTariffChanged(GridTariff.at(t, zone))
+            tariff = GridTariff.at(t, zone)
+            _ <- logger.info(
+              s"At ${t.atZone(zone)}, grid tariff changed to $tariff"
+            )
+          } yield Event.Grid.GridTariffChanged(tariff)
         )
   }
 
