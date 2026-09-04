11affa728dca98de0b28a07f984881d569b8e681
s.gabalda <s.gabalda@stuart.com>
2026-09-04 23:04:20 +0200

    Logs to InfraredStoveDynamicConsumer

--- src/main/scala/calespiga/processor/infraredStove/InfraredStoveDynamicPowerConsumer.scala
+++ src/main/scala/calespiga/processor/infraredStove/InfraredStoveDynamicPowerConsumer.scala
@@ -11,8 +11,12 @@ import calespiga.config.InfraredStoveConfig
 import calespiga.processor.utils.SyncDetector
 import java.time.Instant
 import cats.effect.IO
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 private object InfraredStoveDynamicPowerConsumer {
+
+  private given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
   private case class Impl(
       config: InfraredStoveConfig,
@@ -27,24 +31,26 @@ private object InfraredStoveDynamicPowerConsumer {
         state: State,
         now: Instant
     ): IO[Power] =
-      IO.pure {
         if (state.infraredStove.lastCommandReceived.contains(SetAutomatic)) {
           infraredStoveSyncDetector.checkIfInSync(state) match
             case SyncDetector.NotInSync(since)
                 if now.isAfter(
                   since.plusMillis(config.syncTimeoutForDynamicPower.toMillis)
                 ) =>
-              Power.zero
+              logger.warn(
+                s"Infrared stove is not in sync for dynamic power usage since $since, ignoring dynamic power usage"
+              ).as(Power.zero)
             case _ =>
-              state.infraredStove.status match {
+              val result = state.infraredStove.status match {
                 case Some(InfraredStoveSignal.Power600)  => Power.ofFv(600f)
                 case Some(InfraredStoveSignal.Power1200) => Power.ofFv(1200f)
                 case _                                   => Power.zero
               }
+              logger.info(s"As status is ${state.infraredStove.status}, dynamic power usage for infrared stove is $result").as(result)
         } else {
-          Power.zero
+          logger.info(s"Infrared stove is not in automatic mode, ignoring dynamic power usage").as(Power.zero)
         }
-      }
+      
 
     override def usePower(
         state: State,
@@ -57,33 +63,36 @@ private object InfraredStoveDynamicPowerConsumer {
         ) != SetAutomatic
       ) {
         // infrared stove is not in automatic mode, do not use dynamic power
-        IO.pure(DynamicPowerResult(state, Set.empty, Power.zero))
+        logger.info(s"Infrared stove is not in automatic mode, ignoring dynamic power usage").as(DynamicPowerResult(state, Set.empty, Power.zero))
       } else {
-        val desiredPowerLevel =
-          infraredStoveSyncDetector.checkIfInSync(state) match {
-            case SyncDetector.NotInSync(since)
-                if now.isAfter(
-                  since.plusMillis(config.syncTimeoutForDynamicPower.toMillis)
-                ) =>
-              InfraredStoveSignal.Off
-            case _ =>
-              if (powerToUse.fv > 1200f) InfraredStoveSignal.Power1200
-              else if (powerToUse.fv > 600f) InfraredStoveSignal.Power600
-              else InfraredStoveSignal.Off
-          }
+        for{
+          desiredPowerLevel <-
+            infraredStoveSyncDetector.checkIfInSync(state) match {
+              case SyncDetector.NotInSync(since)
+                  if now.isAfter(
+                    since.plusMillis(config.syncTimeoutForDynamicPower.toMillis)
+                  ) =>
+                logger.info(s"Infrared stove is not in sync for dynamic power usage since $since, setting power to Off").as(InfraredStoveSignal.Off)
+              case _ =>
+                val res = if (powerToUse.fv > 1200f) InfraredStoveSignal.Power1200
+                else if (powerToUse.fv > 600f) InfraredStoveSignal.Power600
+                else InfraredStoveSignal.Off
+                logger.info(s"Desired power level for infrared stove based on available power $powerToUse is $res").as(res)
+            }
+          
+          newState = state
+            .modify(_.infraredStove.lastCommandSent)
+            .setTo(Some(desiredPowerLevel))
 
-        val newState = state
-          .modify(_.infraredStove.lastCommandSent)
-          .setTo(Some(desiredPowerLevel))
 
-        val powerUsed =
-          desiredPowerLevel match {
-            case InfraredStoveSignal.Power1200 => Power.ofFv(1200f)
-            case InfraredStoveSignal.Power600  => Power.ofFv(600f)
-            case _                             => Power.zero
-          }
+          powerUsed =
+            desiredPowerLevel match {
+              case InfraredStoveSignal.Power1200 => Power.ofFv(1200f)
+              case InfraredStoveSignal.Power600  => Power.ofFv(600f)
+              case _                             => Power.zero
+            }
 
-        IO.pure(
+        }yield(
           DynamicPowerResult(
             newState,
             actions.commandActionWithResend(desiredPowerLevel),
