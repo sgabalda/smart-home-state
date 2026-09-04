a9fbc42b0597dcb4a63e93f6e5e7bbf41c6409a0
s.gabalda <s.gabalda@stuart.com>
2026-09-04 22:55:52 +0200

    Logs to HeaterDynamicConsumer

--- src/main/scala/calespiga/processor/heater/HeaterDynamicPowerConsumer.scala
+++ src/main/scala/calespiga/processor/heater/HeaterDynamicPowerConsumer.scala
@@ -11,8 +11,12 @@ import calespiga.config.HeaterConfig
 import calespiga.processor.utils.SyncDetector
 import java.time.Instant
 import cats.effect.IO
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 object HeaterDynamicPowerConsumer {
+
+  private given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
 
   private case class Impl(
       config: HeaterConfig,
@@ -27,26 +31,27 @@ object HeaterDynamicPowerConsumer {
         state: State,
         now: Instant
     ): IO[Power] =
-
-      IO.pure {
         if (state.heater.lastCommandReceived.contains(SetAutomatic)) {
           heaterSyncDetector.checkIfInSync(state) match
             case SyncDetector.NotInSync(since)
                 if now.isAfter(
                   since.plusMillis(config.syncTimeoutForDynamicPower.toMillis)
                 ) =>
-              Power.zero
+              logger.warn(
+                s"Heater is not in sync for dynamic power usage since $since, ignoring dynamic power usage"
+              ).as(Power.zero)
             case _ =>
-              state.heater.status match {
+              val result = state.heater.status match {
                 case Some(HeaterSignal.Power500)  => Power.ofFv(500f)
                 case Some(HeaterSignal.Power1000) => Power.ofFv(1000f)
                 case Some(HeaterSignal.Power2000) => Power.ofFv(2000f)
                 case _                            => Power.zero
               }
+              logger.info(s"As status is ${state.heater.status}, dynamic power usage for heater is $result").as(result)
         } else {
-          Power.zero
+          logger.info(s"Heater is not in automatic mode, ignoring dynamic power usage").as(Power.zero)
         }
-      }
+    
 
     override def usePower(
         state: State,
@@ -59,34 +64,33 @@ object HeaterDynamicPowerConsumer {
         ) != SetAutomatic
       ) {
         // heater is not in automatic mode, do not use dynamic power
-        IO.pure(DynamicPowerResult(state, Set.empty, Power.zero))
+        logger.info(s"Heater is not in automatic mode, ignoring dynamic power usage").as(DynamicPowerResult(state, Set.empty, Power.zero))
       } else {
-        val desiredPowerLevel = heaterSyncDetector.checkIfInSync(state) match {
-          case SyncDetector.NotInSync(since)
-              if now.isAfter(
-                since.plusMillis(config.syncTimeoutForDynamicPower.toMillis)
-              ) =>
-            HeaterSignal.Off
-          case _ =>
-            if (powerToUse.fv > 2000f) HeaterSignal.Power2000
-            else if (powerToUse.fv > 1000f) HeaterSignal.Power1000
-            else if (powerToUse.fv > 500f) HeaterSignal.Power500
-            else HeaterSignal.Off
-        }
+        for{
+          desiredPowerLevel <- heaterSyncDetector.checkIfInSync(state) match {
+            case SyncDetector.NotInSync(since)
+                if now.isAfter(
+                  since.plusMillis(config.syncTimeoutForDynamicPower.toMillis)
+                ) =>
+              logger.info(s"Heater is not in sync for dynamic power usage since $since, setting power to Off").as(HeaterSignal.Off)
+            case _ =>
+              val result = if (powerToUse.fv > 2000f) HeaterSignal.Power2000
+              else if (powerToUse.fv > 1000f) HeaterSignal.Power1000
+              else if (powerToUse.fv > 500f) HeaterSignal.Power500
+              else HeaterSignal.Off
 
-        val newState = state
-          .modify(_.heater.lastCommandSent)
-          .setTo(Some(desiredPowerLevel))
-
-        val powerUsed =
-          desiredPowerLevel match {
-            case HeaterSignal.Power2000 => Power.ofFv(2000f)
-            case HeaterSignal.Power1000 => Power.ofFv(1000f)
-            case HeaterSignal.Power500  => Power.ofFv(500f)
-            case _                      => Power.zero
+              logger.info(s"Desired power level for heater based on available power $powerToUse is $result").as(result)
           }
-
-        IO.pure(
+          newState = state          .modify(_.heater.lastCommandSent)          .setTo(Some(desiredPowerLevel))
+          powerUsed =
+            desiredPowerLevel match {
+              case HeaterSignal.Power2000 => Power.ofFv(2000f)
+              case HeaterSignal.Power1000 => Power.ofFv(1000f)
+              case HeaterSignal.Power500  => Power.ofFv(500f)
+              case _                      => Power.zero
+            }
+          _ <- logger.info(s"Dynamic power used for heater is $powerUsed")
+        }yield(
           DynamicPowerResult(
             newState,
             actions.commandActionWithResend(desiredPowerLevel),
